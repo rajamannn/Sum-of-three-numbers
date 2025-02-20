a89ec491d344075edb2222835936e906e096d884
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2025-02-20 10:10:37 +0530

    Add files via upload

---
+++ Sum_of_three_numbers.java
@@ -0,0 +1,12 @@
+public class Sum_of_three_numbers
+
+{
+    public static void main(String[] args)
+    {
+        System.out.print("The Sum of these numbers: ");
+        int num1 =32;
+        int num2 = 18;
+        int product = num1 + num2;
+      System.out.println(product);
+    }
+}
